36b732edf62fb224c85a0199607d871a8c7dfbb6
Jose Manuel Mendoza <mendozamarin@gmail.com>
2020-04-22 17:46:29 +0200

    prueba completa

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ import path from 'path';
 
 const app = express();
 
+// Conexión base de datos Local
+const mongoose = require('mongoose');
+//const uri = 'mongodb://localhost:27017/mendofydb';
+
+// Conexión base de datos nube
+const uri = 'mongodb+srv://mendo:75LzxKK7aL61cZ4z@mendofydb-xmlh4.mongodb.net/mendofydb?retryWrites=true&w=majority';
+
+const options = {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true};
+
+// Or using promises
+mongoose.connect(uri, options).then(
+  /** ready to use. The `mongoose.connect()` promise resolves to mongoose instance. */
+  () => { console.log('Conectado a DB') },
+  /** handle initial connection error */
+  err => { console.log(err) }
+);
+
 // Middleware
 app.use(morgan('tiny'));
 app.use(cors());
@@ -16,6 +33,8 @@ app.use(express.urlencoded({ extended: true }));
 //app.get('/', (req, res) => {
   //res.send('Hello World!');
 //});
+
+app.use('/api',require('./routes/nota'));
 
 // Middleware para Vue.js router modo history
 const history = require('connect-history-api-fallback');
