698fc5af21214ad7bf184be6f23a3d28463dd81b
jmmm0057 <jmmm0057@red.ujaen.es>
2020-05-20 19:16:08 +0200

    Mendofy Beta 2.0

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ import path from 'path';
 
 const app = express();
 
-// Conexión base de datos Local
 const mongoose = require('mongoose');
+
+// Conexión base de datos Local
 //const uri = 'mongodb://localhost:27017/mendofydb';
 
 // Conexión base de datos nube
@@ -14,7 +15,7 @@ const uri = 'mongodb+srv://mendo:75LzxKK7aL61cZ4z@mendofydb-xmlh4.mongodb.net/me
 
 const options = {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true};
 
-// Or using promises
+// promesas de conecxion con la base de datos
 mongoose.connect(uri, options).then(
   /** ready to use. The `mongoose.connect()` promise resolves to mongoose instance. */
   () => { console.log('Conectado a DB') },
@@ -22,26 +23,23 @@ mongoose.connect(uri, options).then(
   err => { console.log(err) }
 );
 
-// Middleware
+// Middleware para la resolucion de rutas
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
-// Rutas
-//app.get('/', (req, res) => {
-  //res.send('Hello World!');
-//});
 
-
+// Ruta
 app.use('/api',require('./routes/cancion'));
 
-// Middleware para Vue.js router modo history
+// Middleware para Vue.js router modo history(guarda el estado)
 const history = require('connect-history-api-fallback');
 app.use(history());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//puesto por defecto 3000
 app.set('puerto', process.env.PORT || 3000);
 app.listen(app.get('puerto'), () => {
   console.log('Example app listening on port'+ app.get('puerto'));
